c546fbf74c5e5db57be6545986502cda655041ed
GuillemRibas <s2019651@ed.ac.uk>
2024-03-04 18:52:06 +0000

    Quantum error correction test working!

--- ErrorCorrection_test.py
+++ ErrorCorrection_test.py
@@ -21,20 +21,20 @@ for i in range(8):
     temp = np.zeros(8, dtype=complex)
     temp[i] = 1
     base_states.append(temp)
-    base_states_matrices.append(np.outer(temp, temp))
+    base_states_matrices.append(DenseMatrix(np.outer(temp, temp)))
 # print(base_states, base_states_matrices, sep="\n")
 
 
 def trace_register(register):
-    assert (isinstance(register, np.ndarray) and
-            len(register) == 2**10), "A register of 10 qubits is neccessary -> register of 7 qubits + 3 qubit control"
+    assert (isinstance(register.inputArray, np.ndarray) and
+            len(register.inputArray) == 2**10), "A register of 10 qubits is neccessary -> register of 7 qubits + 3 qubit control"
 
     out = np.zeros((2**7, 2**7), dtype=complex)
 
     for i in range(2**7):
         for j in range(2**7):
 
-            old = register[i*2**3:i*2**3+8, j*2**3:j*2**3+8]
+            old = register.inputArray[i*2**3:i*2**3+8, j*2**3:j*2**3+8]
 
             for t in range(3):
                 new = np.zeros((2**(2-t), 2**(2-t)))
@@ -43,7 +43,7 @@ def trace_register(register):
                         new[a, b] = trace_single(old[a*2:a*2+2, b*2:b*2+2])
                 old = new.copy()
             out[i, j] = old[0, 0]
-    return out
+    return DenseMatrix(out)
 
 
 def trace_single(matrix):
@@ -68,3 +68,59 @@ for _ in range(2**10):
     m[_][_] = 1
 input = np.array(m)
 output = trace_register(input)"""
+
+
+Register = Q_Register(10)
+XGate = Gate("Sparse", "spinX")
+CGate = Gate("Sparse","cNot")
+
+Register.apply_gate(XGate, [1,2,3,4,5]) # Error at position with index 5 (qubit 6)
+
+# State is |0111110000>
+
+# First parity check
+paritylist1 = [0,2,4,6] # check qubits in positions 1,3,5,7
+for i in paritylist1:
+    Register.apply_gate(CGate, [i,9])
+
+# Second parity check
+paritylist2 = [1,2,5,6] # check qubits in positions 2,3,6,7
+for i in paritylist2:
+    Register.apply_gate(CGate, [i,8])
+
+# Third parity check
+paritylist3 = [3,4,5,6] # check qubits in positions 4,5,6,7
+for i in paritylist3:
+    Register.apply_gate(CGate, [i,7])
+
+# State should now be in the state |0111110110>
+
+densitymat = DenseMatrix(np.outer(Register.state,Register.state))
+Id = DenseMatrix(np.eye(128))
+errorpos = 0
+
+for i in range(8):
+    TensorProd = TensorProduct([Id,base_states_matrices[i]]).denseTensorProduct()
+    matrix = TensorProd.Multiply(densitymat)
+    if np.any(matrix.DenseApply(np.ones(2**10))) == False:
+        pass
+    else:
+        errorpos += i
+        break
+
+if errorpos == 0:
+    print("There is no error.")
+else:
+    print("The error is in qubit " + str(errorpos))
+
+Register.apply_gate(XGate, [errorpos-1])
+newdensitymat = DenseMatrix(np.outer(Register.state,Register.state))
+
+FinalCleanDensityMat = trace_register(TensorProduct([Id,base_states_matrices[errorpos]]).denseTensorProduct().Multiply(newdensitymat))
+FinalCleanStateVec = FinalCleanDensityMat.DenseApply(np.ones(2**7))
+print(FinalCleanStateVec)
+
+
+
+
+
