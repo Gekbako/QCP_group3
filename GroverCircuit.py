e707752fdde2c5b89a5871da8386e5d123a1c453
Tracque <48903708+Tracque@users.noreply.github.com>
2024-03-04 09:53:08 +0000

    Documented/Sped Up Grover

--- GroverCircuit.py
+++ GroverCircuit.py
@@ -2,84 +2,97 @@ import numpy as np
 
 from Gate_File import Gate
 from Q_Register_File import *
+from LazyMatrix_File import LazyMatrix
 from Tensor import TensorProduct
 
 class GroverCircuit(object):
-    '''
-    Class representing an entire quantum circuit.
+    """
+    Class representing a Grover Circuit
 
     Must contain: 
-        - The full circuit, consisting of: 
-            * Gates, tensored together to account for which qubits are being operated on
-                and then multiplied together (eiter with matmul or Lazy mult.)  
-            * Quantum register being operated on 
-            * Measurement options at the end?
-        - A function to add gates to certain qubits 
-            * This must be optimalised in some way and not just applying one and one gate with
-                a ton of identity matrices tensored to them. 
-        - Visualisation?
-    ''' 
+        - The type of computation
+        - The number of Qubits
+        - The "Target State" of the oracle
+    """ 
 
     def __init__(self, matrixType, numberOfQubits, targetState):
-        '''
-        Notes:
-            This constructor must initialise a Qu Register and a "space to apply gates to". 
+        """
+        This constructor initialises our register and some important variables for the circuit
 
         Input:
         ------
         numberOfQubits [int] - the number of qubits to tensor together to form the register 
-        matrixType [string] - the type of matrix implemented in gates. "dense", "sparse" or "lazy" 
-            --> Should matrixType be global for the class or specific to the gates? I.e. should we be able to mix types? 
+        matrixType [string] - the type of matrix implemented in gates. "Dense", "Sparse" or "Lazy" 
+        targetState [int] - the target of the oracle. (this is a number between 0 and 2^numberOfQubits - 1 inclusive)
 
-        '''
+        """
         self.Register = Q_Register(numberOfQubits)
         self.Dimension = 2**numberOfQubits 
         self.NumberOfQubits = numberOfQubits
-        self.GateArray = [ []*n for n in range(numberOfQubits)] # array to keep track of applied gates for visualisation
         self.MatrixType = matrixType
         self.targetState = targetState
 
-
-        if matrixType == "dense":
+        '''
+        Unused Visualization tracking
+        
+        self.GateArray = [ []*n for n in range(numberOfQubits)] # array to keep track of applied gates for visualisation
+        
+        if matrixType == "Dense":
             self.CircuitChain = DenseMatrix(np.identity(self.Dimension))
-        elif matrixType == "sparse":
+        elif matrixType == "Sparse":
             self.CircuitChain = SparseMatrix(np.identity(self.Dimension)) 
-        elif matrixType == "lazy":
-            #???
-            pass
-
-
-         # maybe this needs to be initialised to 1 (identity) such 
-                                                                # that we can multiply gates into it from the start? 
-        
-
-    ###########################################################################################
-        # Alternative way to add gates to circuit:
-        #I modififed these functions to use construct for creating tensor products (then just gonna use apply)
-
-    def AddGate(self, gate : str, qubit):
+        elif matrixType == "Lazy":
+            self.CircuitChain = LazyMatrix(np.identity(self.Dimension))
         '''
-        Function to add Gate to Circuit by use of Apply method from Q_Register. 
-        Also keeps track of which gates have been added to circuit for visualisation.   
-        '''
-        addedGate = Gate(self.MatrixType, gate)
-        self.Register = self.Register.apply_gate(addedGate, qubit)
+    
+    def getHadamards(self):
+        """
+        Function to initialise the matrix for applying a hadamard to every quBit
         
-        #Visualization Step
-        if gate == "cNot" or gate == "cV":
-            self.GateArray[qubit[0]].insert(0, gate + "-control")
-            self.GateArray[qubit[1]].insert(0, gate + "-target")
+        Input
+        ------
+        self
+        """
+        
+        hadamardList = []
+        
+        for i in range(self.NumberOfQubits):
+            if self.MatrixType != "Lazy":
+                hadamardList.append(Gate(self.MatrixType, gateName = "hadamard").GateMatrix)
+            else:
+                hadamardList.append(Gate("Sparse", gateName = "hadamard").GateMatrix)      
+            
+        if self.MatrixType == "Dense":
+            TensoredHadamards = TensorProduct(hadamardList).denseTensorProduct
+            
+            TensoredH = TensoredHadamards().denseTensorProduct()
+            
+        elif self.MatrixType == "Sparse" or self.MatrixType == "Lazy":
+            TensoredHadamards = TensorProduct(hadamardList)
+            
+            TensoredH = TensoredHadamards.sparseTensorProduct()
+        
+        if self.MatrixType != "Lazy":
+            self.TensoredH = TensoredH
+        else:
+            self.TensoredH = LazyMatrix(self.Dimension, TensoredH.SparseApply)
 
-        else: self.GateArray[qubit].insert(0, gate)
-
-    def getOracle(self, targetState):
+    def getOracle(self):
         """
         Function to create the oracle matrix/operator
+        
+        Input
+        ------
+        self
+        
+        Returns
+        ------
+        oracleMatrix [Dense/Sparse/Lazy Matrix] - The oracle matrix, which flips a target state
         """
         
         if self.MatrixType == "Dense":
             oracleMatrix = np.eye(self.Dimension)
-            oracleMatrix[targetState][targetState] = -1
+            oracleMatrix[self.targetState][self.targetState] = -1
             
             oracleMatrix = DenseMatrix(oracleMatrix)
             
@@ -88,57 +101,94 @@ class GroverCircuit(object):
             oracleMatrixElements = []
             
             for i in range(self.Dimension):
-                if i == targetState:
+                if i == self.targetState:
                     oracleMatrixElements.append([i,i,-1])
                 else:
                     oracleMatrixElements.append([i,i,1])
                 
                 oracleMatrix = SparseMatrix(self.Dimension, oracleMatrixElements)
                 
+        elif self.MatrixType == "Lazy":
+            
+            oracleMatrixElements = []
+            
+            for i in range(self.Dimension):
+                if i == self.targetState:
+                    oracleMatrixElements.append([i,i,-1])
+                else:
+                    oracleMatrixElements.append([i,i,1])
+                
+                oracleSparse = SparseMatrix(self.Dimension, oracleMatrixElements)
+                
+                oracleMatrix = LazyMatrix(self.Dimension, oracleSparse.SparseApply)
+                
         return oracleMatrix
 
     def getFullyMixedState(self):
+        """
+        Function to create the fully mixed state.
+        
+        Input
+        ------
+        self
+        
+        Returns
+        ------
+        fullyMixedState [vector] - The fully mixed state (which has equal probabilities of being measured in any state)
+        """
         
         Register = Q_Register(self.NumberOfQubits)
         
-        hadamardList = []
-        
-        for i in range(self.NumberOfQubits):
-            hadamardList.append(Gate(self.MatrixType, gateName = "hadamard").GateMatrix)
-            
         if self.MatrixType == "Dense":
-            TensoredHadamards = TensorProduct(hadamardList).denseTensorProduct
-            
-            fullyMixedState = TensoredHadamards.DenseApply(Register.state)
-            
-        elif self.MatrixType == "Sparse":
-            TensoredH = TensorProduct(hadamardList)
-            
-            TensoredHadamards = TensoredH.sparseTensorProduct()
-            
-            fullyMixedState = TensoredHadamards.SparseApply(Register.state.inputArray)
+            fullyMixedState = self.TensoredH.DenseApply(Register.state.inputArray)
+        else:
+            fullyMixedState = self.TensoredH.SparseApply(Register.state.inputArray)
         
         return fullyMixedState
 
     def getGrover(self):
+        """
+        Function to create the grover matrix/operator
+        
+        Input
+        ------
+        self
+        
+        Returns
+        ------
+        groverMatrix [list of Dense/Sparse/Lazy Matrix] - The grover matrix, which inverts qubit values around the mean
+            Note:
+                groverMatrix is returned as a list of matrices, so we can avoid performing expensive matrix matrix multiplication.
+                We use that grover is given by H diag(1,-1,-1,...,-1) H with H being hadamards applied to every quBit.
+        """
         
         groverState = self.getFullyMixedState()
         
         if self.MatrixType == "Dense":
-            groverMatrix = -np.eye(self.Dimension)
+            groverTemp = -np.eye(self.Dimension)
             
-            for i in range(self.Dimension):
-                groverMatrix[i][i] += 2 * groverState[i]
+            groverTemp[0][0] = 1
                 
-            groverMatrix = DenseMatrix(groverMatrix)
+            groverMatrix = [self.TensoredH, DenseMatrix(groverTemp), self.TensoredH]
                 
         elif self.MatrixType == "Sparse":
+            #groverTemp is the diag(1,-1,-1,-1,...,-1,-1) matrix
+            groverTemp = []
+                
+            for i in range(self.Dimension):
+                groverTemp.append([i,i,-1])
+                
+            groverTemp[0] = [0,0,1] 
+            
+            groverMatrix = [self.TensoredH, SparseMatrix(self.Dimension, groverTemp), self.TensoredH]
+            
+        elif self.MatrixType == "Lazy":
             groverTemp = []
             
             hadamardList = []
         
             for i in range(self.NumberOfQubits):
-                hadamardList.append(Gate(self.MatrixType, gateName = "hadamard").GateMatrix)
+                hadamardList.append(Gate("Sparse", gateName = "hadamard").GateMatrix)
                 
             for i in range(self.Dimension):
                 groverTemp.append([i,i,-1])
@@ -146,25 +196,29 @@ class GroverCircuit(object):
             groverTemp[0] = [0,0,1] 
             
             groverTemp = SparseMatrix(self.Dimension, groverTemp)
+            
+            groverLazy = LazyMatrix(self.Dimension, groverTemp.SparseApply)
                 
             TensoredH = TensorProduct(hadamardList)
             
             TensoredHadamards = TensoredH.sparseTensorProduct()
             
-            ExtraTemp = TensoredHadamards
+            LazyHadamards = LazyMatrix(self.Dimension, TensoredHadamards.SparseApply)
             
-            groverMatrix = ExtraTemp.Multiply(groverTemp)
+            groverMatrix = LazyHadamards.multiply(groverLazy)
             
-            groverMatrix = groverMatrix.Multiply(TensoredHadamards)
-            
-            """
-            This implementation is super hacky since I was just trying to check that it works. (Also I've only implemented sparse rn)
-            Essentially, I use that the grover operator is given by H diag(1,-1,-1,...,-1) H, with H being the fully tensored hadamards
-            """
+            groverMatrix = groverMatrix.multiply(LazyHadamards)
             
         return groverMatrix
             
     def iterateGrover(self):
+        """
+        Function to perform one grover iteration
+        
+        Input
+        ------
+        self
+        """
             
         if self.MatrixType == "Dense":
                 
@@ -180,56 +234,68 @@ class GroverCircuit(object):
             print(self.Register.state)
             
             #Apply the Oracle
-            self.Register.state = self.oracle.SparseApply(self.Register.state)
+            self.Register.state = self.oracle.SparseApply(self.Register.state)  
             
             print("State after Oracle")
             print(self.Register.state)
             
             #Apply the Grover
-            self.Register.state = self.grover.SparseApply(self.Register.state)
+            self.Register.state = self.grover[0].SparseApply(self.Register.state)
+            self.Register.state = self.grover[1].SparseApply(self.Register.state)
+            self.Register.state = self.grover[2].SparseApply(self.Register.state)
             
             print("State after Grover")
             print(self.Register.state)
             
             
 
-    ###########################################################################################
-        # WAIT! THE QU REGISTER HAS THE APPLY METHOD DEFINED...USE THIS
-
-    ###########################################################################################
-
-
-    def visualiseCircuit(self):
-        '''
-        Function to make graphic visualisation of what's going on. Most useful to make sense of what is applied where 
-        in the circuit chain. 
-        No idea how to implement.
-        Function should be run each time a gate is added to show the updated circuit? 
-        '''
-        pass
-
     def runGrover(self):
-        '''
-        Function to run the circuit. Returns either endstate or result of measurement? Or both? 
-        '''
+        """
+        Function to run the full grover circuit
+        
+        Input
+        ------
+        self
+        
+        Returns
+        ------
+        finalState [vector] - The register state after applying the grover iteration ~ pi/4 * root(2^numOfQuBits) times
+        measuredState [vector] - The state collapsed to upon measurement
+        result [int] - The number represented by measuredState. This is most likely to equal targetState
+        """
         
         #Initialise the register and the oracle/grover operators.
-        self.Register.state = self.getFullyMixedState()
+        self.getHadamards()
+        
+        if self.MatrixType != "Lazy":
+            self.Register.state = self.getFullyMixedState()
+        else:
+            self.fullGrover = LazyMatrix(self.Dimension, self.TensoredH.SparseApply)
+            
         self.grover = self.getGrover()
-        self.oracle = self.getOracle(self.targetState)
+        self.oracle = self.getOracle()
         
         numIterations = int(np.pi / 4 * np.sqrt(self.Dimension))
         
-        for i in range(numIterations):
-            self.iterateGrover()
+        if self.MatrixType != "Lazy":
+            for i in range(numIterations):
+                self.iterateGrover()
+        else:
+            for i in range(numIterations):
+                self.fullGrover = self.oracle.multiply(self.fullGrover)
+                self.fullGrover = self.grover.multiply(self.fullGrover)
+                 
+            self.fullGrover.Apply(self.Register.state)
             
+        #Extract the result
         finalState = self.Register.state
         result = self.Register.measure()
         measuredState = self.Register.state
         
         return finalState, measuredState, result
-    
-circuit = GroverCircuit("Sparse", 3, 6)
+
+#Demonstration of the circuit
+circuit = GroverCircuit("Sparse", 8, 200)
 state, measuredState, result = circuit.runGrover()
 
 print(state)
